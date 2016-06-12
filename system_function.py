5c6bea3e4841fa2e6404e8deb1848da9acb8cb1c
xda-linux <xda-linux@qq.com>
2016-06-12 19:11:05 +0800

    add dir function

--- system_function.py
+++ system_function.py
@@ -1,3 +1,15 @@
-import os
+import os,sys
 basedir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 print basedir
+
+print os.path.dirname(__file__)
+#get your file's directory name
+
+print __file__
+#get your file name
+
+print os.path.abspath('.')
+#get the currently directory
+
+print sys.stdout.encoding
+#get your system coding
