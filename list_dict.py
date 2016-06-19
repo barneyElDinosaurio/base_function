ff11abc3bf781e9f699130c96aedfdf1b9a82d39
Rockyzsu <jinweizsu@gmail.com>
2016-06-19 11:40:51 +0800

    add dict assign

--- list_dict.py
+++ list_dict.py
@@ -1,33 +1,51 @@
 import collections
-s=[("yellow",1),("red",2),("white",3),("black",4),("green",5)]
-d=collections.defaultdict(list)
-for k,v in s:
-    print "k is %s, v is %d" %(k,v)
-    #d[k]=v
+def list_dict():
+    s=[("yellow",1),("red",2),("white",3),("black",4),("green",5)]
+    d=collections.defaultdict(list)
+    for k,v in s:
+        print "k is %s, v is %d" %(k,v)
+        #d[k]=v
 
-    d[k].append(v)
+        d[k].append(v)
 
-print d
+    print d
 
-print d
-for k in d:
-    print "k is %s, v is " %k
-    print d[k][0]
+    print d
+    for k in d:
+        print "k is %s, v is " %k
+        print d[k][0]
 
-print "*"*10
-s='mississippi'
-d=collections.defaultdict(int)
-for k in s:
-    #print k
-    d[k]+=1
+    print "*"*10
+    s='mississippi'
+    d=collections.defaultdict(int)
+    for k in s:
+        #print k
+        d[k]+=1
 
-print d
+    print d
 
-print 'I\'m {},{}'.format("hongten","welcome to you")
+    print 'I\'m {},{}'.format("hongten","welcome to you")
 
-mydict=collections.defaultdict(list)
-s=[("a",[1,2,3,4]),("b",[2,3,4,5]),("c",[3,4,5,6]),("d",[4,5,6,7])]
-for k,v in s:
-    mydict[k]=v
+    mydict=collections.defaultdict(list)
+    s=[("a",[1,2,3,4]),("b",[2,3,4,5]),("c",[3,4,5,6]),("d",[4,5,6,7])]
+    for k,v in s:
+        mydict[k]=v
 
-print mydict
+    print mydict
+
+
+def dict_assign():
+    a={}
+    b=a
+    a["a"]="1"
+    print b
+    x=1
+    y=x
+    x=2
+    print y
+    a["a"]="2"
+    print b
+    print id(a)
+    print id(b)
+
+dict_assign()
