3e18c00b0cd0774e14f55c90d62655af530b406f
Rockyzsu <jinweizsu@gmail.com>
2016-07-09 12:42:29 +0800

    add two files

---
+++ calc.py
@@ -0,0 +1,3 @@
+for i in range(1,6):
+    rise=(1.1**i-1)*100
+    print "%d day's raise is %.2f" %(i,rise)
---
+++ random_test.py
@@ -0,0 +1,10 @@
+__author__ = 'rocky'
+import random
+#list=[1,2,3,4,5]
+#print list[2]
+
+list=[]
+for i in range(536870911):
+    list.append(random.random())
+
+print len(list)
