c0c908ed887f24ea1c49c06a04074edf0c02cd8a
rocky v3k <jinweizsu@139.com>
2016-07-15 09:06:38 +0800

    update

--- tushare_function.py
+++ tushare_function.py
@@ -1,7 +1,7 @@
 #-*-coding=utf-8-*-
 __author__ = 'rocchen'
 import tushare as ts
-
+import datetime
 #df=ts.get_hist_data('300141',start='2011-01-01',end='2016-7-13')
 #这个函数只能获取近3年的数据
 #print df
@@ -20,6 +20,12 @@ date_format=data[0:4]+'-'+data[4:6]+'-'+data[6:8]
 print date_format
 #日期的格式进行转换
 
+day0=datetime.date(datetime.date.today().year,datetime.date.today().month,datetime.date.today().day)
+day30=day0-datetime.timedelta(60)
+print day30
+day30=day30.strftime("%Y-%m-%d")
+day0=day0.strftime("%Y-%m-%d")
+
 '''
 data="20101112"
 index=0
@@ -29,7 +35,19 @@ for i in data:
     index=index+1
 print data[1:3]
 '''
-df = ts.get_h_data('300141',start=date_format,end="2016-07-12")
+df = ts.get_h_data('300141',start=day30,end=day0)
 #这个函数可以获取所有的历史数据
-print df
-print df.dtypes
+#print df
+highest= df['high']
+lowest=df['low']
+price_30_max = highest.max()
+price_30_min = lowest.min()
+print price_30_max
+print price_30_min
+#oneData= df.ix['2016-07-11']
+#print oneData.iloc[0,1]
+#print type(oneData)
+for i in highest.len:
+    print i
+
+#print type(t)
