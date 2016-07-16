9695ae836249c55ade582606a5930874892cabb7
Rockyzsu <jinweizsu@gmail.com>
2016-07-16 18:57:52 +0800

    update

--- mongo_test.py
+++ mongo_test.py
@@ -2,7 +2,12 @@
 __author__ = 'rocchen'
 import pymongo
 client=pymongo.MongoClient('localhost',1999)
-db=client.test
+db=client.test1234
 print db.name
 print db.my_collection
 u=dict(name="user1",age=23)
+user=db.users
+db.users.insert(u)
+
+
+print "done"
---
+++ sqlite_test.py
@@ -0,0 +1,5 @@
+#-*-coding=utf-8-*-
+__author__ = 'rocchen'
+import sqlite3
+conn=sqlite3.connect("rocky_sqlite.db")
+print "open database passed"
