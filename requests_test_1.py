41850aa1c696c308a8ebe9bb0d6517319add2b72
Rockyzsu <jinweizsu@gmail.com>
2016-08-15 18:57:23 +0800

    add request 1

--- requests_test_1.py
+++ requests_test_1.py
@@ -7,11 +7,15 @@ def request_test():
     url='https://github.com/timeline.json'
     r=requests.get(url)
     print r.text
+    print r.json()
 
     data={"name":"Rocky"}
     url2="http://httpbin.org/post"
     s=requests.post(url2,data)
     print s.text
+    print s.url
+    t=requests.post(url2,params=data)
+    print t.url
 
 
 request_test()
