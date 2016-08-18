2f67a1a7b77a07f4fd01d1b797cacd84c7e20d8f
Rockyzsu <jinweizsu@gmail.com>
2016-08-18 09:06:45 +0800

    add eval_test

--- eval_test.py
+++ eval_test.py
@@ -1,1 +1,12 @@
+#-*-coding=utf-8-*-
 __author__ = 'Rocky'
+
+def eval_test():
+    exp="2*10"
+    result=eval(exp)
+    print result
+
+    a=raw_input("Please input a exp:\n")
+    t=eval(a)
+    print t
+eval_test()
