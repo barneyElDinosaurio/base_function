6f2aab14ce6c82fb14a4e9cb610945d103589332
Rockyzsu <jinweizsu@gmail.com>
2017-01-03 18:54:55 +0800

    update

--- mysql_test.py
+++ mysql_test.py
@@ -5,8 +5,10 @@ __author__ = 'xda'
 #sys.setdefaultencoding('utf-8')
 from sqlalchemy import create_engine
 import tushare as ts
-
+import read_config
 df=ts.get_tick_data('300333',date='2016-12-27')
 print df
-engine=create_engine('mysql://root:temp@localhost/mydb?charset=utf8')
-df.to_sql('tick_data1',engine)
+account=read_config.getUserData()
+mysql_password=account['mysql_password']
+engine=create_engine('mysql://root:%s@localhost/mydb?charset=utf8' %mysql_password)
+df.to_sql('tick_data5',engine)
