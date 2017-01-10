040d5d51b7c1134499ddf96b957b2d33de730cd4
=rocky mba <jinweizsu@gmail.com>
2017-01-10 11:48:24 +0800

    update

--- mysql_test.py
+++ mysql_test.py
@@ -8,5 +8,5 @@ import tushare as ts
 
 df=ts.get_tick_data('300333',date='2016-12-27')
 print df
-engine=create_engine('mysql://root:temp@localhost/mydb?charset=utf8')
+engine=create_engine('mysql://root:123456z@localhost/mydb?charset=utf8')
 df.to_sql('tick_data1',engine)
