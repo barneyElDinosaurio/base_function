1719428a100ad01f4100d281a7c73c4ed7fcd9e7
Rockyzsu <jinweizsu@gmail.com>
2017-01-19 09:58:37 +0800

    update

--- uiautomator_usage.py
+++ uiautomator_usage.py
@@ -1,19 +1,30 @@
 # -*-coding=utf-8-*-
 __author__ = 'Rocky'
-
+import time,os
+import subprocess
 from uiautomator import device as d
 #注意这个device是小写的
 
 def basic_usage():
-
+    '''
     info= d.info
     print info
     print type(info)
     for i in info:
         #print v
         print i,info[i]
+    '''
+    while True:
+        info=d.info
+        print info['currentPackageName']
+        time.sleep(10)
+    #com.taobao.taobao
+    #com.jingdong.app.mall
+    #com.suning.mobile.ebuy
+    #com.kingpoint.gmcchh
+    #com.jd.jrapp
 
-    print info['currentPackageName']
+
 
 def operation_usage():
 
@@ -40,6 +51,24 @@ def operation_usage():
 
     d(scrollable=True).scroll(steps=3)
 
+def launch_app():
+    cmd='adb shell am start -n com.kingpoint.gmcchh/.ui.home.StartUpActivity'
+    #os.system(cmd)
+    p=subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+    print p.stdout.read()
+    if d(text=u'版本更新').wait.exists(timeout=15000):
+        d(text=u'取消').click()
+    else:
+        print "Latest version"
+    d(scrollable=True).scroll.to(text=u'签到赢话费')
+    d(text=u'签到赢话费').click()
+    print "Go"
+    time.sleep(3)
+    d(text=u'签到').click()
+    #d(text=u'')
+    #print result
+
 if __name__=="__main__":
     #basic_usage()
-    operation_usage()
+    #operation_usage()
+    launch_app()
