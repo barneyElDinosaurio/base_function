46fac45478240802bc50542cf14c88d285ce6711
Rockyzsu <jinweizsu@gmail.com>
2017-02-23 16:53:06 +0800

    t430

--- requests_test.py
+++ requests_test.py
@@ -8,8 +8,9 @@ session = requests.session()
 
 session.cookies = cookielib.LWPCookieJar(filename="cookies")
 agent = 'Mozilla/5.0 (Windows NT 5.1; rv:33.0) Gecko/20100101 Firefox/33.0'
-headers = {'Host': 'www.zhihu.com',
-           'Referer': 'https://www.zhihu.com',
+headers = {'Host': 'xueqiu.com',
+           'Referer': 'https://xueqiu.com/',
+           'Origin':'https://xueqiu.com',
            'User-Agent': agent}
 '''
 #url='http://www.zhihu.com'
---
+++ toolkit.py
@@ -0,0 +1,24 @@
+# -*-coding=utf-8-*-
+#常用的工具集合
+__author__ = 'Rocky'
+
+
+class Toolkit():
+    @staticmethod
+    def save2file( filename, content):
+        # 保存为文件
+        filename = filename + ".txt"
+        f = open(filename, 'a')
+        f.write(content)
+        f.close()
+    @staticmethod
+    def getUserData(cfg_file):
+        f=open(cfg_file,'r')
+        account={}
+        for i in f.readlines():
+            ctype,passwd=i.split('=')
+            #print ctype
+            #print passwd
+            account[ctype.strip()]=passwd.strip()
+
+        return account
