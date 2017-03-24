86dfdf2d0cf5127f286c09f6d24768f42959a9c1
Rockyzsu <jinweizsu@gmail.com>
2017-03-25 00:45:45 +0800

    update

--- function_test.py
+++ function_test.py
@@ -9,7 +9,12 @@ def getData(name):
 def path_function():
     print os.getcwd()
 
+def print_fun():
+    a=1
+    #print '%d100/%\n' %a
+    print "a values is ",a,"%"
 #getData('Rocky',1000)
 #wrong!
 
-path_function()
+#path_function()
+print_fun()
--- list_test.py
+++ list_test.py
@@ -1,27 +1,28 @@
 # -*-coding=utf-8-*-
 __author__ = 'Rocky'
 import json
-a=[1,2,3,4]
-b=[54,3,2,1,2]
-c=a+b
-print c
-d=list(set(c))
-print d
+def case1():
+    a=[1,2,3,4]
+    b=[54,3,2,1,2]
+    c=a+b
+    print c
+    d=list(set(c))
+    print d
 
 
-urlss = ['http://bj.xiaozhu.com/search-duanzufang-p{}-0/'.format(str(i)) for i in range(1,6,1)]
-#print urlss
-for i in urlss:
-    print i
+    urlss = ['http://bj.xiaozhu.com/search-duanzufang-p{}-0/'.format(str(i)) for i in range(1,6,1)]
+    #print urlss
+    for i in urlss:
+        print i
 
-record=[json.loads(line) for line in open('json.txt')]
-print record
-print '\n'
-'''
-for line in open('json.txt'):
-    print line
-    print "\n"
-'''
+    record=[json.loads(line) for line in open('json.txt')]
+    print record
+    print '\n'
+    '''
+    for line in open('json.txt'):
+        print line
+        print "\n"
+    '''
 
 def getCount(strings):
     counts={}
@@ -67,4 +68,14 @@ count=getCount(recordx)
 print count['a']
 modify_list()
 '''
-in_test()
+
+def delete_item_list():
+    x=[[1,2],[3,4],[5,6],[7,8]]
+    for i in x:
+        #print i
+        if 5 in i:
+            x.remove(i)
+    print x
+
+#in_test()
+delete_item_list()
