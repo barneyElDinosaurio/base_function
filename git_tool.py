96f6707ae9b3af44a711aeff9ace96821f54e719
Rockyzsu <jinweizsu@gmail.com>
2017-06-01 09:18:58 +0800

    git tool

---
+++ git_tool.py
@@ -0,0 +1,40 @@
+# -*-coding=utf-8-*-
+__author__ = 'Rocky'
+'''
+http://30daydo.com
+Contact: weigesysu@qq.com
+'''
+import subprocess,os
+def git_sync():
+    current=os.getcwd()
+    fetch='git fetch origin'
+    status='git status'
+    for (dirname,dirs,filename) in os.walk('.'):
+        print dirname
+        print dirs
+        print filename
+        for dir in dirs:
+            path=os.path.join(current,dir)
+            os.chdir(path)
+            print os.getcwd()
+            try:
+
+                print path
+                #print os.listdir('.')
+                os.system('git status')
+                p=subprocess.Popen(fetch,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+                p.communicate()
+                p.wait()
+                print p.stdout.read()
+                p=subprocess.Popen(status,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+                p.communicate()
+                p.wait()
+                print p.stderr.read()
+            except Exception,e:
+                print e
+        break
+
+    #print os.listdir(current)
+
+
+git_sync()
--- plot_test.py
+++ plot_test.py
@@ -104,6 +104,33 @@ def two_in_one_canvas():
     vol.plot()
     plt.show()
 
+def line_define():
+    labels='frogs','hogs','dogs','logs'
+    sizes=15,20,45,10
+    colors='yellowgreen','gold','lightskyblue','lightcoral'
+    explode=0,0.1,0,0
+    #画的是饼状图
+    plt.pie(sizes,explode=explode,labels=labels,colors=colors,autopct='%1.1f%%',shadow=True,startangle=50)
+    plt.axis('equal')
+    plt.show()
+
+def hist_test():
+    mu, sigma = 100, 15
+    x = mu + sigma * np.random.randn(10000)
+
+    # 数据的直方图
+    n, bins, patches = plt.hist(x, 50, normed=1, facecolor='g', alpha=0.75)
+
+
+    plt.xlabel('Smarts')
+    plt.ylabel('Probability')
+    #添加标题
+    plt.title('Histogram of IQ')
+    #添加文字
+    plt.text(60, .025, r'$mu=100, sigma=15$')
+    plt.axis([40, 160, 0, 0.03])
+    plt.grid(True)
+    plt.show()
 #from_book()
 #plot_test1()
 #pd_plot()
@@ -113,5 +140,6 @@ def two_in_one_canvas():
 #multi_plot()
 #plot_csdn()
 
-two_in_one_canvas()
-
+#two_in_one_canvas()
+#line_define()
+hist_test()
