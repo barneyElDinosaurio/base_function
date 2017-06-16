72948130495dec180d64e32f8dfce80cfe2cf92f
Rocky Chen <weigesysu@qq.com>
2017-06-16 09:22:18 +0800

    面试题

--- calc.py
+++ calc.py
@@ -8,7 +8,11 @@ def test1():
         print "%d day's raise is %.2f" % (i, rise)
 
 
-c = yield_calc(3.65, 1000000)
-print c
+#c = yield_calc(3.65, 1000000)
+#print c
 
-print round(17.955, 2)
+#print round(17.955, 2)
+def percentage(a,b):
+    print (a-b)*1.00/b*100
+
+percentage(0.195,0.185)
