2ad0536b7606833c5d331f261271e3487738b833
Rock Chen <weigesysu@qq.com>
2017-06-22 10:51:50 +0800

    正则表达式验证和hash验证

--- regular_test.py
+++ regular_test.py
@@ -6,3 +6,10 @@ org_str = u"我是一名中国人<br>吗</br>？Helo<br>World</br>"
 print org_str
 dest_str = re.sub('<br>|</br>', '\n', org_str)
 print dest_str
+
+t1='i love baseketball 13424281084!'
+#phone_number=re.search
+
+text = 'c++ python2 python3 perl ruby lua javascript java php4 php5 c'
+print re.match(r'java',text)
+print re.search(r'java',text).group()
