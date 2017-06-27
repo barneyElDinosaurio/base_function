77ed58289151084cc20bfc3328d3ca83e6a19366
unknown <rocchen@ROCCHEN-LT.nvidia.com>
2017-06-27 18:51:16 +0800

    my wrapper

--- decorator.py
+++ decorator.py
@@ -1,6 +1,7 @@
+#-*-coding=utf-8-*-
 import logging
 
-
+'''
 def get_parameter(func):
     print "IN GET PARAMETER"
 
@@ -17,13 +18,38 @@ def show_function():
     print "SHOW Function"
     return "HELLO"
 
-
+'''
 # a=show_function
 #a()
 #get_parameter()
 #a=show_function()
 #print a
+
+#演示2个装饰器同时使用
+def add_br(func):
+    #print '<br>',
+
+    def wrapper():
+        return '<br>'+func()+'</br>'
+    return wrapper
+
+def add_header(func):
+    def wrapper():
+        return "<header>" +func()+ '</header>'
+    return wrapper
+
+@add_header
+@add_br
+def sayHelloWorld():
+    return "Hello world！"
+
+
+def main():
+    print sayHelloWorld()
+
 if __name__ == "__main__":
-    logging.info("Test")
+    #logging.info("Test")
     #print "Test"
-    show_function()
+    #show_function()
+    #sayHello()
+    main()
