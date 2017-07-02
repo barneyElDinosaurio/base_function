ac94b125a46ceb41d88953f8c8aac23104a56c31
Rocky Chen <weigesysu@qq.com>
2017-07-02 21:20:07 +0800

    add interview

--- decorator.py
+++ decorator.py
@@ -44,9 +44,41 @@ def sayHelloWorld():
     return "Hello world！"
 
 
-def main():
-    print sayHelloWorld()
+def deco1(func):
+    def wrapper(arg1,arg2):
+        print "Start"
+        func(arg1,arg2)
+        print "End"
+    return wrapper
 
+@deco1
+def args_deco_test(x1,x2):
+    print "first args is %s . Second args is %s." %(x1,x2)
+
+
+def multi_args(func):
+    def wrapper(*args,**kwargs):
+        print "start"
+        func(*args,**kwargs)
+        print "end"
+    return wrapper
+
+@multi_args
+def deco_multi_arg(*args,**kwargs):
+    for i in args:
+        print "args : %s" %i
+
+    for k in kwargs:
+        print "key=" ,k
+        print 'value=',kwargs[k]
+    print "end in main"
+
+
+def main():
+    #print sayHelloWorld()
+    print "Morning"
+    #args_deco_test('one','two')
+    deco_multi_arg('args1','args2','args3','args4',name="Rocky",sex="Female")
 if __name__ == "__main__":
     #logging.info("Test")
     #print "Test"
