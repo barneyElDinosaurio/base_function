489abc4a8bdf1a894c84797c1db3c497e27c5d6c
Rock Chen <weigesysu@qq.com>
2017-07-13 17:16:48 +0800

    偏函数

--- function_test.py
+++ function_test.py
@@ -1,5 +1,5 @@
 import os
-
+from functools import partial
 
 def getData(name, times):
     print "Your name is", name, " Times is :", times
@@ -23,9 +23,21 @@ def loopTest():
     for i in range(-9, 10, 1):
         print i
 
+def sayHi(x,name):
+    return x+name
+
+def main():
+    p=partial(sayHi,name='Rocky')
+    print p('Chen')
+    print p('Li')
+    print p('Peng')
+    print p('Luo')
+
+main()
+
 # getData('Rocky',1000)
 #wrong!
 
 #path_function()
 #print_fun()
-loopTest()
+#loopTest()
