d9b109e387d830cf89676ef62dc0bb775eb2a5ba
[Rocky Chen] <jinweizsu@gmail.com>
2017-07-18 00:58:10 +0800

    redist

---
+++ count_function.py
@@ -0,0 +1,5 @@
+#-*-coding=utf-8-*-
+import requests
+def count_words(url):
+    resp=requests.get(url)
+    return len(resp.text.split())
---
+++ distrubted.py
@@ -0,0 +1,38 @@
+#-*-coding=utf-8-*-
+#import requests
+import time
+
+from rq import Queue
+from redis import Redis
+from count_function import count_words
+
+
+#print count_words('http://nvie.com')
+
+'''
+def main():
+    redis_conn=Redis()
+    q=Queue(connection=redis_conn)
+    job=q.enqueue(count_words,'http://nvie.com')
+    print job.result
+
+    time.sleep(5)
+    print job.result
+'''
+
+#redis_conn=Redis(host='192.168.2.234',port=6379,socket_connect_timeout=5)
+redis_conn=Redis(host='192.168.2.234',port=6379)
+q=Queue(connection=redis_conn)
+job=q.enqueue(count_words,'http://nvie.com')
+print job.result
+
+time.sleep(5)
+print job.result
+
+
+
+
+'''
+if __name__ == '__main__':
+    main()
+'''
