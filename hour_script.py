9a0c7aac0e54943b157d94cdb7f6334a4719ce18
Rock Chen <weigesysu@qq.com>
2017-07-19 18:21:41 +0800

    auto script

---
+++ hour_script.py
@@ -0,0 +1,31 @@
+# -*-coding=utf-8-*-
+__author__ = 'Rocky'
+'''
+http://30daydo.com
+Contact: weigesysu@qq.com
+'''
+from functools import partial
+import datetime
+def save_file(content,file_name):
+    try:
+        f=open(file_name,'a')
+        f.write(content)
+        f.write('\n')
+        f.close()
+    except Exception,e:
+        print e
+        return
+
+def running_log():
+    now=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    content=now+"  execute the script"
+    filename='logfile.txt'
+    to_file=partial(save_file,file_name=filename)
+    to_file(content)
+
+
+def main():
+    running_log()
+
+if __name__=='__main__':
+    main()
---
+++ logging_usage.py
@@ -0,0 +1,26 @@
+# -*-coding=utf-8-*-
+__author__ = 'Rocky'
+'''
+http://30daydo.com
+Contact: weigesysu@qq.com
+'''
+
+import logging,sys
+LEVELS = {'debug': logging.DEBUG,
+          'info': logging.INFO,
+          'warning': logging.WARNING,
+          'error': logging.ERROR,
+          'critical': logging.CRITICAL}
+
+
+level_name = 'info'
+level = LEVELS.get(level_name, logging.NOTSET)
+logging.basicConfig(level=level)
+'''
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical error message')
+'''
+
