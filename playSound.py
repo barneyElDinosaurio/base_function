c14a7466d2d0f1c942cb3fc7513b3167ed083645
[Rocky Chen] <jinweizsu@gmail.com>
2017-07-19 22:39:22 +0800

    add alert

--- playSound.py
+++ playSound.py
@@ -1,21 +1,8 @@
 # play sound in python
 import os, subprocess
+media_path = "ios.wav"
 
-media_path = "/Users/rocky/Data/shutter.wav"
-cmd1 = "mplayer %s" % media_path
-cmd = 'uname -a'
-print cmd
-'''
-result = os.popen(cmd1)
-print result.read()
-status = os.system(cmd1)
-print status
-'''
+p = subprocess.Popen(['mplayer', media_path], stdout=subprocess.PIPE)
+p.communicate()
+p.wait()
 
-myresult = subprocess.Popen(['mplayer', media_path], stdout=subprocess.PIPE)
-myresult.wait()
-
-print myresult.stdout.read()
-
-print os.times()
-print os.uname()
