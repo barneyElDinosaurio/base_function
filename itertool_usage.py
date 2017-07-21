ca34e54c3e958cf34ec4d72dfbe9b6d2733520d9
unknown <rocchen@ROCCHEN-LT.nvidia.com>
2017-07-21 19:19:10 +0800

    排列组合

---
+++ itertool_usage.py
@@ -0,0 +1,11 @@
+# -*-coding=utf-8-*-
+__author__ = 'Rocky'
+'''
+http://30daydo.com
+Contact: weigesysu@qq.com
+'''
+from itertools import permutations
+a=['a','b','c','d']
+for i in permutations(a,2):
+    print i[0],i[1]
+
