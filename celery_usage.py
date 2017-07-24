c598bce96b0cfdc280f03e02240426253450f231
Rock Chen <weigesysu@qq.com>
2017-07-24 11:38:41 +0800

    celery test case

---
+++ celery_usage.py
@@ -0,0 +1,16 @@
+# -*-coding=utf-8-*-
+__author__ = 'Rocky'
+'''
+http://30daydo.com
+Contact: weigesysu@qq.com
+'''
+from celery import Celery
+ip='10.19.133.255'
+broker='redis://%s:7777/5'
+backend='redis://%s:7777/6'
+
+app=Celery('tasks',broker=broker,backend=backend)
+
+@app.tasks
+def add(x,y):
+    return x+y
