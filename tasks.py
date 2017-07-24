81c65157a6727c54e0d9142c16bd6a5501d59bf0
Rock Chen <weigesysu@qq.com>
2017-07-24 11:51:35 +0800

    change name

--- tasks.py
+++ tasks.py
@@ -9,8 +9,8 @@ ip='10.19.133.255'
 broker='redis://%s:7777/5'
 backend='redis://%s:7777/6'
 
-app=Celery('celery_usage',broker=broker,backend=backend)
+app=Celery('tasks',broker=broker,backend=backend)
 
-@app.celery_usage
+@app.tasks
 def add(x,y):
     return x+y
