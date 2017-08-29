077aacf1e973d32f086a241323797cc86eeca165
chenjw@laptop <chenjw@jpush.cn>
2017-08-29 09:01:34 +0800

    update

--- lxml_test.py
+++ lxml_test.py
@@ -53,8 +53,59 @@ def lxml_text():
 def lxml_case():
     r=requests.get('http://30daydo.com')
     t=etree.HTML(r.text)
-    s=t.xpath('//div[@class="aw-item article"]/)
+    s=t.xpath('//div[@class="aw-item article"]')
     print s
 #lxml_text()
 
-lxml_case()
+#lxml_case()
+
+def lxml_case2():
+    #网站的例子
+    str1='''
+    <bookstore>
+
+    <book>
+      <title>Harry Potter</title>
+      <author>J K. Rowling</author>
+      <year>2005</year>
+      <price>29.99</price>
+    </book>
+    
+    </bookstore>
+    '''
+    tree=etree.HTML(str1)
+    t1=tree.xpath('bookstore')
+    print t1
+
+def lxml_case3():
+
+
+    text = '''
+    <div>
+        <ul>
+             <li class="item-0"><a href="link1.html">first item</a></li>
+             <li class="item-1"><a href="link2.html">second item</a></li>
+             <li class="item-inactive"><a href="link3.html">third item><span>Hello world</span></a></li>
+             <li class="item-1"><a href="link4.html">fourth item</a></li>
+             <li class="item-0"><a href="link5.html">fifth item</a>
+             <li class="de-item-0"><a href="link5.html">fifth item</a>
+         </ul>
+     </div>
+    '''
+
+    tree=etree.HTML(text)
+    html_s=etree.tostring(tree)
+    #print html_s
+    #print tree.xpath('//li//span/text()')[0]
+    '''
+    reg_case=tree.xpath('//*[starts-with(@class,"item")]')
+    for i in reg_case:
+        print i.xpath('.//a/@href')
+    '''
+    result=tree.xpath(r'//*[re:match(@class, "item-0")]')
+    print result
+
+    for i in result[0]:
+        print i.xpath('.//a/@href')
+
+lxml_case3()
--- redis_test.py
+++ redis_test.py
@@ -12,28 +12,38 @@ def base_usage():
     print r.dbsize()
     print r.exists('house')
     #r = redis.Redis(host='183.232.57.39', port=7001, db=0,password='jiguang')
-    '''
+
     for i in range(10):
         r.set(i,i)
-    
+        #每次执行不会覆盖
+
     for i in range(10):
         x=r.get(i)
         print x
         print type(x)
     #print r.get('test')
-    '''
+
 
 def insert_data():
+    '''
     key='house'
     value=['shenzhen','欧陆经典','2000','160户']
+    r.set(key,value)
+    '''
+    key='dict'
+    value={'name':'rocky','age':26,'sex':'f'}
     r.set(key,value)
 
 def get_data():
     keys=r.keys()
-    print keys
-    for key in keys:
-        print r.get(key),'type ', type(r.get(key))
+    # 返回的事key的列表
 
-base_usage()
+    print 'key:',keys,
+
+    for key in keys:
+        print key," ",r.get(key),'type ', type(r.get(key))
+
+
+#base_usage()
 #insert_data()
-#get_data()
+get_data()
