4be3d141e62c1ced3520e46a8328f8a0e5506652
chenjw <chenjw@jpush.cn>
2017-08-31 08:43:34 +0800

    update

--- clear_city.py
+++ clear_city.py
@@ -1,15 +1,15 @@
 # coding: utf-8
 import re
-fp=open('city.txt','r').read()
+fp=open('temp_city.txt','r').read()
 #print fp
 s=re.sub(r'/\* \d+ \*/','',fp)
 #dic=eval(s)
-print s
-s1=re.findall('"_id" : "(.*?)"',s)
+#print s
+s1=re.findall('"city_name" : "(.*?)"',s)
 s2=re.findall('"count" : (\d+)',s)
-
+print s2
 for i in range(len(s1)):
-    print s1[i], " ", s2[i]
+    print s1[i], " ", s2[i],"/",s2[i]
 
 '''
 ss=0
