a60228a23a17b35add86881de08f5b70d8785e92
Rockyzsu <jinweizsu@gmail.com>
2017-10-11 23:12:15 +0800

    dict locate

--- dictionary_usage.py
+++ dictionary_usage.py
@@ -1,53 +1,76 @@
-#-*-coding=utf-8-*-
+# -*-coding=utf-8-*-
 import random
 
-keys=[chr(i) for i in range(ord('A'),ord('Z')+1)]
 
-print keys
+def base_usage():
+    keys = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
 
-values=[random.randint(0,100) for i in range(len(keys))]
+    print keys
 
-print values
+    values = [random.randint(0, 100) for i in range(len(keys))]
 
-d=zip(keys,values)
-print d
-print type(d)
-d1=dict(d)
-print d1
+    print values
 
-k=sorted(d1.items(),key=lambda x:x[1],reverse=True)
-print k
+    d = zip(keys, values)
+    print d
+    print type(d)
+    d1 = dict(d)
+    print d1
 
-CUSTOMER_FROM_NAMES = (
-    (1, u"无"),
-    (2, u"首页登记"),
-    (3, u"客服QQ"),
-    (4, u"邮件"),
-    (5, u"市场活动"),
-    (6, u"电话咨询"),
-    (7, u"电话外呼"),
-    (8, u"商务扩展"),
-    (9, u"内部推荐"),
-    (10, u"公司分配"),
-)
+    k = sorted(d1.items(), key=lambda x: x[1], reverse=True)
+    print k
 
-l=CUSTOMER_FROM_NAMES
-l2= l[1:9]
-l3=l2+l[:1]
-print l3
-print type(l[0])
-print type(l[:1])
-print l[0]
-print l[:1]
 
-print "*"*10
+def choice_usage():
+    CUSTOMER_FROM_NAMES = (
+        (1, u"无"),
+        (2, u"首页登记"),
+        (3, u"客服QQ"),
+        (4, u"邮件"),
+        (5, u"市场活动"),
+        (6, u"电话咨询"),
+        (7, u"电话外呼"),
+        (8, u"商务扩展"),
+        (9, u"内部推荐"),
+        (10, u"公司分配"),
+    )
+
+    l = CUSTOMER_FROM_NAMES
+    l2 = l[1:9]
+    l3 = l2 + l[:1]
+    print l3
+    print type(l[0])
+    print type(l[:1])
+    print l[0]
+    print l[:1]
+
+
+# 字典按照键或者值来排序
 def sort_usge():
-    dic={'a':33,'b':2,'c':44,'z':22,'d':89,'e':5}
+    dic = {'a': 33, 'b': 2, 'c': 44, 'z': 22, 'd': 89, 'e': 5}
     print dic
-    for k,v in dic.items():
-        print k,v
-    dic2 = sorted(dic.iteritems(),key=lambda asd:asd[0],reverse=True)
+    for k, v in dic.items():
+        print k, v
+    dic2 = sorted(dic.iteritems(), key=lambda asd: asd[0], reverse=True)
     print dic
     print dic2
 
-sort_usge()
+
+# 根据值来找键
+def find_key():
+    # 创建一个随机的字典
+    keys = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
+    vals = [random.randint(1, 100) for _ in range(len(keys))]
+    d = dict(zip(keys, vals))
+
+    print d
+    p = 55
+    for k, v in d.items():
+        if p == v:
+            print k
+            break
+
+
+# sort_usge()
+# base_usage()
+find_key()
