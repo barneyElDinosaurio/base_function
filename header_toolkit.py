2de8d51eb01588073ad5f7d3b2bb4613f0816ecb
Rockyzsu <jinweizsu@gmail.com>
2017-10-14 21:39:55 +0800

    update lagou

--- header_toolkit.py
+++ header_toolkit.py
@@ -19,11 +19,15 @@ def getheader():
 
 def analysis_cookie():
     cookie = getheader()['Cookie']
+    print cookie
+    items = cookie.split(';')
     for item in items:
-        name = item.split('=')[0]
+        name  = item.split('=')[0]
         value = item.split('=')[1]
-        print name, " : ", value
-
+        #print name,value
+        name=name.replace(' ','')
+        #print '\'',name,'\'',':','\'',value,'\'',','
+        print '\'{}\':\'{}\','.format(name,value)
 
 def urlParse():
     url = 'http://lg.snssdk.com/api/news/feed/v66/?concern_id=6286225228934679042&refer=1&count=20&max_behot_time=1506210972&last_refresh_sub_entrance_interval=1506220325&loc_mode=0&loc_time=1506071873&latitude=22.552248&longitude=113.903644&city=%E6%B7%B1%E5%9C%B3%E5%B8%82&tt_from=pre_load_more&lac=9365&cid=3672&cp=5493c07313925q1&plugin_enable=3&st_time=333&iid=15189530186&device_id=38220008232&ac=wifi&channel=smartisan&aid=13&app_name=news_article&version_code=636&version_name=6.3.6&device_platform=android&ab_version=179886%2C169863%2C177250%2C179334%2C173967%2C172663%2C172659%2C171194%2C170349%2C178991%2C176179%2C177070%2C169447%2C178210%2C179194%2C168463%2C174398%2C178732%2C178921%2C169300%2C178930%2C180698%2C177166%2C152026%2C176593%2C180172%2C179891%2C178581%2C177786%2C170713%2C179373%2C176739%2C179006%2C156262%2C145585%2C180654%2C179382%2C174429%2C177258%2C180186%2C177042%2C162572%2C176599%2C176609%2C179625%2C175163%2C169176%2C175634%2C176616%2C170988%2C178989%2C176597%2C176652%2C177702%2C176615&ab_client=a1%2Cc4%2Ce1%2Cf2%2Cg2%2Cf7&ab_feature=102749%2C94563&abflag=3&ssmix=a&device_type=SM801&device_brand=SMARTISAN&language=zh&os_api=22&os_version=5.1.1&uuid=990006203070023&openudid=4dd00e258bbe295f&manifest_version_code=636&resolution=1080*1920&dpi=480&update_version_code=6368&_rticket=1506220325881&plugin=2431'
@@ -114,8 +118,8 @@ def debug_page():
 
 
 #tree,text = debug_page()
-print getheader()
-    # analysis_cookie()
+#print getheader()
+analysis_cookie()
     # urlParse()
     # read_json()
     # print urlAdd('urlparse')
--- lagou.py
+++ lagou.py
@@ -1,30 +1,54 @@
 # -*-coding=utf-8-*-
+import random
+
 import requests
+import time
 
 
 def _postdata():
     url = 'https://www.lagou.com/gongsi/searchPosition.json'
 
     # 最小header
-    headers = {
-               'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
-               'Referer': 'https://www.lagou.com/gongsi/j917.html',
-               }
+    headers = {'Origin': 'https://www.lagou.com', 'Content-Length': '87', 'Accept-Language': 'zh,en;q=0.8,en-US;q=0.6', 'Accept-Encoding': 'gzip, deflate, br', 'X-Anit-Forge-Code': '33435644', 'X-Requested-With': 'XMLHttpRequest', 'X-Anit-Forge-Token': '5b95a0d3-eed5-49e9-a5e7-f02be38a2716', 'Host': 'www.lagou.com', 'Accept': 'application/json, text/javascript, */*; q=0.01', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36', 'Connection': 'keep-alive', 'Cookie': 'user_trace_token=20160612223035-3a02d006-30aa-11e6-a343-5254005c3644; LGUID=20160612223035-3a02d566-30aa-11e6-a343-5254005c3644; JSESSIONID=ABAAABAACDBAAIAF33B4C87F595BE43FE5DBD6846EA46FE; _gat=1; PRE_UTM=; PRE_HOST=; PRE_SITE=; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2F; _putrc=DFAF58A586D854B0; login=true; unick=%E9%99%88%E9%94%A6%E4%BC%9F; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; hasDeliver=124; TG-TRACK-CODE=index_search; SEARCH_ID=78ff057aeb2b4837979c6966e1840c32; index_location_city=%E6%B7%B1%E5%9C%B3; _gid=GA1.2.1335763621.1507978370; _ga=GA1.2.499625224.1465741835; LGSID=20171014185251-d2e2289c-b0cd-11e7-9550-5254005c3644; LGRID=20171014185300-d8c73858-b0cd-11e7-94ed-525400f775ce; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1507809365,1507978370; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1507978380', 'Pragma': 'no-cache', 'Cache-Control': 'no-cache', 'Referer': 'https://www.lagou.com/gongsi/j917.html', 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
 
-    data = {'companyId': 917,
+
+    data = {'companyId': '917',
             'positionFirstType': u'全部',
-            'schoolJob': False,
-            'pageNo': 1,
-            'pageSize': 10}
+            'schoolJob': 'False',
+            'pageNo': '1',
+            'pageSize': '10'}
+
+    for i in range(1,10):
+        data['pageNo']=str(i)
+        r = requests.post(url=url, data=data, headers=headers)
+        print r.status_code
+        js = r.json()
+        #totalCount = js.get('content').get('data').get('page').get('totalCount')
+        try:
+            print r.text
+            results = js.get('content').get('data').get('page').get('result')
+            #time.sleep(random.random()*10)
+        except:
+            print "can't find result"
+            print i
+            continue
+    #print totalCount
+    #for i in results:
+    '''
+    i=7
+    data['pageNo']=str(i)
     r = requests.post(url=url, data=data, headers=headers)
     print r.status_code
     js = r.json()
-    totalCount = js.get('content').get('data').get('page').get('totalCount')
-    results = js.get('content').get('data').get('page').get('result')
-    print totalCount
-    for i in results:
-
-
+    print r.text
+    #totalCount = js.get('content').get('data').get('page').get('totalCount')
+    try:
+        results = js.get('content').get('data').get('page').get('result')
+    except:
+        print "can't find result"
+        #print i
+        #continue
+    '''
 
 
 _postdata()
