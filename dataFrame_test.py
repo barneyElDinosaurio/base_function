685880afe80bc47fbcdd506f6e46cee5bb867b97
Rockyzsu <jinweizsu@gmail.com>
2017-12-31 02:00:25 +0800

    up

--- dataFrame_test.py
+++ dataFrame_test.py
@@ -1,26 +1,30 @@
 # -*-coding=utf-8-*-
+import time
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from pandas import Series
-import matplotlib.pyplot as plt
+
 from setting import get_engine
-import time
-import timeit
-#显示全部的行
-pd.set_option('display.max_rows',None)
+
+# 显示全部的行
+pd.set_option('display.max_rows', None)
+
 
 def df_practice():
     a = [1, -23, 4, 5, 6, 7, -4, 34, 3, 5, 33]
     b = [-2, 55, -5, 99, 3, -3, 55, 3, -1, 4, 7]
     df = pd.DataFrame({'A': a, 'B': b})
     print df
-    #df.loc[(df['A']>0) & (df['B']<0),'A']=100
-    #赋值测试
-    #df.ix[(df['A'] > 0) & (df['B'] < 0), 'A'] = 100
+    # df.loc[(df['A']>0) & (df['B']<0),'A']=100
+    # 赋值测试
+    # df.ix[(df['A'] > 0) & (df['B'] < 0), 'A'] = 100
     print df
     print df.ix[0, 0]
     print df.dtypes
     print df['A'].idxmax()
+
 
 # 计算相关系数
 def corr_number():
@@ -29,9 +33,9 @@ def corr_number():
     if (b / a)[0] == 2:
         print "ok"
     c = [2, 4, 6, 8, 10, 12, 14, 16, 18]
-    d = [i * 2  for i in c]
+    d = [i * 2 for i in c]
     print d
-    #d[0] = 3
+    # d[0] = 3
     s1 = Series(c)
     s2 = Series(d)
 
@@ -41,8 +45,8 @@ def corr_number():
 
 def plot_test():
     s = Series(np.random.rand(20))
-    #s.plot()
-    #plt.show(s.plot(kind='hist'))
+    # s.plot()
+    # plt.show(s.plot(kind='hist'))
     ax = s.plot(kind='bar')
     fig = ax.get_figure()
     fig.savefig('1.png')
@@ -75,33 +79,33 @@ def calculate():
     count = len(df[(df['changepercent'] >= -10.2) & (df['changepercent'] < -9)])
     total.append(count)
     for i in range(-9, 9, 1):
-        #print i
-        #print df[(df['changepercent']>=i) & (df['changepercent']<(i+1))]
+        # print i
+        # print df[(df['changepercent']>=i) & (df['changepercent']<(i+1))]
         count = len(df[(df['changepercent'] >= i * 1.00) & (df['changepercent'] < ((i + 1)) * 1.00)])
-        #print count
+        # print count
         total.append(count)
-        #time.sleep(60)
-        #total.append(count)
+        # time.sleep(60)
+        # total.append(count)
     count = len(df[(df['changepercent'] >= 9)])
     total.append(count)
-    #print df
+    # print df
     sum = 0
     for i in range(len(total)):
         sum = sum + total[i]
     print sum
     print total
-    #plt.style.use('ggplot')
+    # plt.style.use('ggplot')
     fig1 = plt.figure(len(total))
     df_figure = pd.Series(total, index=[range(-10, 10)])
     print df_figure
-    #fg=df_figure.plot(kind='bar',title="2017-04-12",grid=True,table=True,)
+    # fg=df_figure.plot(kind='bar',title="2017-04-12",grid=True,table=True,)
     mycolor = ['g'] * 10
     mycolor2 = ['r'] * 10
     mycolor3 = list(mycolor, )
     mycolor.append(mycolor2)
     print len(total)
     fg = df_figure.plot(kind='bar', table=True)
-    #autolabel(fg)
+    # autolabel(fg)
     plt.show(fg)
 
 
@@ -126,58 +130,71 @@ def apply_map_test():
     print df2
 
 
-#行 合并
+# 行 合并
 def row_merge():
     df = pd.DataFrame({'A': [1, 2, 3, 4, 5], 'B': [10, 20, 30, 40, 50]})
     df2 = pd.DataFrame({'A': [6], 'B': [60]})
+    print 'df\n', df
+    print 'df2\n', df2
+
     df_x = [df, df2]
     result = pd.concat(df_x)
-    result = result.reset_index(drop=True)
-    print result
-    print df['A'][0]
+    print 'first result\n', result
+    result2 = result.reset_index(drop=True)
+    print 'origin resutl\n',result
+    print 'second result\n',result2
+    # print df['A'][0]
+    result3 = result.reindex(columns=['A','C'])
+    print 'result3\n',result3
+
 
 def dataframe_create():
-    df=pd.DataFrame()
-    df['name']=[i for i in range(1,10)]
+    df = pd.DataFrame()
+    df['name'] = [i for i in range(1, 10)]
     print df
-    df['id']=[chr(i) for i in range(ord('a'),ord('a')+9)]
+    df['id'] = [chr(i) for i in range(ord('a'), ord('a') + 9)]
     print df
-    df = df.set_index('name',drop=False)
+    df = df.set_index('name', drop=False)
     print df
 
+
 def dataframe_create1():
-    l=10
-    today=1
+    l = 10
+    today = 1
     code = 110
-    name ='hello'
+    name = 'hello'
     opens = 1
-    close =10
-    high=0
-    low=0
-    vol=99
-    amount=99
-    df = pd.DataFrame(columns=[ 'datetime', 'code', 'name', 'open', 'close', 'high', 'low', 'vol', 'amount'])
-    df.loc[99] = [ today, code, name, opens, close, high, low, vol, amount]
+    close = 10
+    high = 0
+    low = 0
+    vol = 99
+    amount = 99
+    df = pd.DataFrame(columns=['datetime', 'code', 'name', 'open', 'close', 'high', 'low', 'vol', 'amount'])
+    df.loc[99] = [today, code, name, opens, close, high, low, vol, amount]
     print df
     engine = get_engine('test')
-    #df.to_sql('test',engine,if_exists='append')
+    # df.to_sql('test',engine,if_exists='append')
+
+
 # 统计出现的次数
 def count_case():
-    df=pd.DataFrame()
-    df['A']=[90,97,95,99,99,99,92,90,97,96,99,99,92,93]
-    df['B']=[80,87,85,89,89,89,82,80,87,86,89,89,82,83]
-    #print df
+    df = pd.DataFrame()
+    df['A'] = [90, 97, 95, 99, 99, 99, 92, 90, 97, 96, 99, 99, 92, 93]
+    df['B'] = [80, 87, 85, 89, 89, 89, 82, 80, 87, 86, 89, 89, 82, 83]
+    # print df
     print df['A'].value_counts()
+
 
 def nan_case():
     df = pd.DataFrame()
-    df['A'] = [1,2,3,None,4,5]
+    df['A'] = [1, 2, 3, None, 4, 5]
     # print df
     print df['A'].value_counts()
-    df['B']=[2,2,2,2,2,2]
+    df['B'] = [2, 2, 2, 2, 2, 2]
     # print df
     print df['A'].sum()
     print df['B'].sum()
+
 
 def misc():
     vals1 = np.array([1, np.nan, 3, 4])
@@ -189,21 +206,20 @@ def misc():
         start = time.time()
         print np.arange(1E8, dtype=dtype).sum()
 
-        print time.time()-start
+        print time.time() - start
+
 
 def main():
-
-    #df_practice()
-    #corr_number()
-    #plot_test()
-    #calculate()
-    #compare_values()
-    #jianshu()
-    #apply_map_test()
-    # row_merge()
-    #dataframe_create1()
+    # df_practice()
+    # corr_number()
+    # plot_test()
+    # calculate()
+    # compare_values()
+    # jianshu()
+    # apply_map_test()
+    row_merge()
+    # dataframe_create1()
     # count_case()
     # nan_case()
-    misc()
-
+    # misc()
 main()
