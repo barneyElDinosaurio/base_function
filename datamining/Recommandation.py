0916ea98f00da8f31dcea4df71577468917d001a
Rockyzsu <jinweizsu@gmail.com>
2018-01-23 19:39:56 +0800

    data ming

---
+++ datamining/Recommandation.py
@@ -0,0 +1,69 @@
+#-*-coding=utf8-*-
+import json,math
+
+class recommander():
+
+	def __init__(self,data,k=3,metric='pearson',n=5):
+		self.data=data
+		self.k=k
+		self.metric=metric
+
+		if self.metric=='pearson':
+			self.fn=self.pearson
+
+	def pearson(self,rating1,rating2):
+		xy=0
+		sumX=0
+		sumY=0
+		sqr_X=0
+		sqr_Y=0
+		n=0
+
+		for key in rating1:
+			if key in rating2:
+				xy+=rating1[key]*rating2[key]
+				sumX+=rating1[key]
+				sumY+=rating2[key]
+				sqr_X+=rating1[key]**2
+				sqr_Y+=rating2[key]**2
+				n+=1
+		denominator=math.sqrt(sqr_X-sumX**2/float(n))*math.sqrt(sqr_Y-sumY**2/float(n))
+		# not capture the denominator is zero !
+		if denominator==0:
+			return 0
+		else:
+			return (xy-sumX*sumY/float(n))/float(denominator)
+
+	def computeNeighbour(self,username):
+		distance=[]
+		for key in self.data:
+			if key!=username:
+				d=self.fn(self.data[username],self.data[key])
+				distance.append((key,d))
+
+		return sorted(distance,key=lambda x:x[1],reverse=True)
+
+	def recommand(self,user):
+		distances=self.computeNeighbour(user)
+		total=0
+		pearson_val=[]
+		for i in range(self.k):
+			name=distances[i][0]
+			pearson_val.append([[name,distances[i][1]])
+		total=pearson_val.sum()
+		# pearson_val=pearson_val
+		persons=[]
+		for per in pearson_val:
+			percent=per[1]/float(total)
+			persons.append((per[0],percent))
+
+		
+
+def main():
+	with open('user.json','r') as f:
+		users=json.load(f)
+	obj=recommander(data=users)
+	print obj.computeNeighbour('Chan')
+
+if __name__ == '__main__':
+	main()
--- datamining/chapter2.py
+++ datamining/chapter2.py
@@ -1,5 +1,5 @@
 #-*-coding=utf-8-*-
-import json
+import json,math
 #load data
 with open('user.json','r') as f:
 	user=json.load(f)
@@ -19,9 +19,49 @@ def computeNeighbor(username,user,r):
 		if k!=username:
 			distance=MinkovskiDistance(user,username,k,2)
 			recommands.append({k:distance})
-	print recommands
+	# print recommands
 	return sorted(recommands,key=lambda x:x.values() ,reverse=True)
 
+def pearson(user1,user2):
+	xy=0
+	sumX=0
+	sumY=0
+	sqr_X=0
+	sqr_Y=0
+	n=0
 
-# print MinkovskiDistance(user,'Angelica','Veronica',2)
-print computeNeighbor('Bill',user,2)
+	for key in user1:
+		if key in user2:
+			xy+=user1[key]*user2[key]
+			sumX+=user1[key]
+			sumY+=user2[key]
+			sqr_X+=user1[key]**2
+			sqr_Y+=user2[key]**2
+			n+=1
+
+	# not capture the denominator is zero !
+	if math.sqrt(sqr_X-sumX**2/float(n))*math.sqrt(sqr_Y-sumY**2/float(n))==0:
+		return 0
+	else:
+		return (xy-sumX*sumY/float(n))/float(math.sqrt(sqr_X-sumX**2/float(n))*math.sqrt(sqr_Y-sumY**2/float(n)))
+
+
+def cosin(user1,user2):
+	sumXY=0
+	x_len=0
+	y_len=0
+	for key in user1:
+		if 
+
+
+def main():
+	# print MinkovskiDistance(user,'Angelica','Veronica',2)
+	# print computeNeighbor('Bill',user,2)
+	# print pearson(user['Angelica'],user['Bill'])
+	# print pearson(user['Angelica'],user['Hailey'])
+	print pearson(user['Angelica'],user['Jordyn'])
+
+
+
+if __name__ == '__main__':
+	main()
