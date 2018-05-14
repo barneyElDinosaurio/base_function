76bde63d408db3ac437d0bb4f19a2b2ee975c037
Rocky Chen <weigesysu@qq.com>
2018-05-15 00:16:15 +0800

    update

---
+++ designMode/adapter_mode.py
@@ -0,0 +1,6 @@
+class Computer:
+	
+
+	def play():
+		print 'run a program'
+
---
+++ designMode/prototype_mode.py
@@ -0,0 +1,28 @@
+import copy
+
+class A:
+	def __init__(self):
+		self.x=10
+		# self.y=20
+		self.msg = 'msg'
+class B(A):
+	def __init__(self):
+		A.__init__(self)
+		self.y=20
+
+
+	def __str__(self):
+		return 'x : {} y: {}'.format(self.x,self.y)
+
+a=A()
+b=B()
+c=b
+# c=copy.deepcopy(b)
+# print b
+print [str(i) for i in [b,c]]
+print [id(i) for i in [b,c]]
+
+b.y=30
+
+print 'b',b
+print 'c',c
