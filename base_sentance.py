8e92088800815488f34e19944aa221d9df893b2c
Rocky Chen <weigesysu@qq.com>
2018-07-14 12:12:33 +0800

    update

--- base_sentance.py
+++ base_sentance.py
@@ -5,13 +5,13 @@ import urllib2
 try:
     #Normal execution block
     open('data.cfg', 'r')
-except urllib2.UnknownHandler, e:
+except urllib2.UnknownHandler as e:
     #Exception A handle
     print("can't find")
-except urllib2.URLError, e:
+except urllib2.URLError as e:
     #Exception B handle
     print("still error")
-except urllib2.UnknownHandler, e:
+except urllib2.UnknownHandler as e:
     #Other exception handle
     print("error")
 else:
@@ -19,3 +19,6 @@ else:
     print("open")
 finally:
     print("finally")
+
+import six
+print(six.PY2)
