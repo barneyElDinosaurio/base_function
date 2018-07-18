b0389c7952d2cb3ee5dec81ac65b276da88a200c
Rocky Chen <jinweizsu@gmail.com>
2018-07-18 15:16:55 +0800

    update

--- sxr_md5.py
+++ sxr_md5.py
@@ -1,5 +1,7 @@
 # -*-coding=utf-8-*-
 import pymysql
+import time
+
 import setting
 
 connect = pymysql.connect(host=setting.MYSQL_HOST, port=setting.MYSQL_PORT, user=setting.MYSQL_USER,
@@ -7,34 +9,66 @@ connect = pymysql.connect(host=setting.MYSQL_HOST, port=setting.MYSQL_PORT, user
 
 
 def losecredit():
-    name = '6591ee8fc212f28d070f377e4cd052c9'
-    idnum = 'dacee6aa0491b2f0485301f7b125aec2'
+    name = '3486df4a8d4b21cfab42a1f56d52313e'
+    idnum = '3622060fa9d08684b088f8769d8ee5c5'
     cursor = connect.cursor()
     cmd1 = 'select * from dishonest t where t.fnameGbkMd5=\'{}\''.format(name)
     cursor.execute(cmd1)
     ret = cursor.fetchall()
     if not ret:
-        print("none")
+        print("None")
         return
 
     for item in ret:
         flag = item[-1]
-        # print(flag)
+        # flag 为 1 身份证完整
         if flag == 1:
-            pass
+            cmd_1 = '''select t.fname,t.idNum from dishonest t where t.fnameGbkMd5=\'{0}\' and t.idNumMd5=\'{}\''''.format(
+                name, idnum)
+            cursor.execute(cmd_1)
+            ret2 = cursor.fetchall()
         else:
             first_of_idnum = int(idnum[0], 16)
             if first_of_idnum == 0:
-                cmd2 = '''SELECT t.fname,t.idNum FROM dishonest t WHERE t.fnameGbkMd5 = \'{0}\' AND EXISTS (SELECT  1 FROM  credit t1 WHERE  t1.idNumMd5 = \'{1}\' AND CONCAT(LEFT(t1.idNum, 4),RIGHT(t1.idNum, 4)) = CONCAT(LEFT(t.idNum, 4),RIGHT(t.idNum, 4)))'''.format(
+                cmd_2 = '''SELECT t.fname,t.idNum FROM dishonest t WHERE t.fnameGbkMd5 = \'{0}\' AND EXISTS (SELECT  1 FROM  credit t1 WHERE  t1.idNumMd5 = \'{1}\' AND CONCAT(LEFT(t1.idNum, 4),RIGHT(t1.idNum, 4)) = CONCAT(LEFT(t.idNum, 4),RIGHT(t.idNum, 4)))'''.format(
                     name, idnum)
             else:
-                cmd2 = '''SELECT t.fname,t.idNum FROM dishonest t WHERE t.fnameGbkMd5 = \'{0}\' AND EXISTS (SELECT  1 FROM  credit{1} t1 WHERE  t1.idNumMd5 = \'{2}\' AND CONCAT(LEFT(t1.idNum, 4),RIGHT(t1.idNum, 4)) = CONCAT(LEFT(t.idNum, 4),RIGHT(t.idNum, 4)))'''.format(
+                cmd_2 = '''SELECT t.fname,t.idNum FROM dishonest t WHERE t.fnameGbkMd5 = \'{0}\' AND EXISTS (SELECT  1 FROM  credit{1} t1 WHERE  t1.idNumMd5 = \'{2}\' AND CONCAT(LEFT(t1.idNum, 4),RIGHT(t1.idNum, 4)) = CONCAT(LEFT(t.idNum, 4),RIGHT(t.idNum, 4)))'''.format(
                     name, first_of_idnum, idnum)
 
-            # print(cmd2)
-            cursor.execute(cmd2)
+            cursor.execute(cmd_2)
             ret2 = cursor.fetchall()
-            print(len(ret2))
+        if ret2:
+            id_set = set()
+            for i in ret2:
+                # print(i)
+                name_decode = i[0]
+                id_set.add(i[1])
+
+            id_list = list(id_set)
+            query_set = []
+            for i in id_list:
+                query_set.append("\""+i+"\"")
+
+            query_sets = ','.join(query_set)
+            cmd_3 = '''SELECT DISTINCT t.cidno,t.fname, t.region, t.case_time, t.case_no, t.court, t.basis_no, t.detail,  t.fullfil, t.publish_time FROM   dw_person_dishonest t where  t.fname=\'{}\' and t.cidno in({})'''.format(name_decode,query_sets)
+            cursor.execute(cmd_3)
+            result = cursor.fetchall()
+            colums=['cidno','fname','region','case_time','case_no','court','basis_no','detail','fullfil','publish_time']
+            resp=[]
+
+            for j in result:
+                print('*****'*5)
+                d={}
+                for index, value in enumerate(j):
+                    d[colums[index]]=value
+                resp.append(d)
+            return resp
+        else:
+            print("can not find the idnum")
+            return None
 
 
-losecredit()
+
+ret = losecredit()
+print(ret)
