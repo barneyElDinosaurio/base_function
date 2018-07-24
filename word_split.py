75b8d595482971f5160ba1e6ad16c27b60ef6f9e
Rockyzsu <jinweizsu@gmail.com>
2018-07-25 00:16:52 +0800

    gensim

---
+++ word_split.py
@@ -0,0 +1,14 @@
+from gensim.models import Word2Vec
+from gensim.models.word2vec import LineSentence
+
+filename = ''
+sentences = LinSentence(filename)
+model = Word2Vec(sentences,size=128,window=5,min_count=5,workers=4)
+model.save('word_embedding_128')
+
+items = model.most_similar('中国')
+for item in items:
+	print(item[0],item[1])
+
+
+print(model.similarity('男人','女人'))
