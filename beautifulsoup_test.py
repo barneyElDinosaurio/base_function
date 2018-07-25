d152dff15e80cd82b0e81b99f5342aef0c4e4e8a
Rockyzsu <jinweizsu@gmail.com>
2018-07-26 00:11:09 +0800

    update

--- beautifulsoup_test.py
+++ beautifulsoup_test.py
@@ -1,28 +1,33 @@
 # -*-coding=utf-8-*-
-__author__ = 'rocky'
-from bs4 import BeautifulSoup
-import urllib2, sys, StringIO, gzip, time, random, re, urllib, os, codecs
+# python2
+# __author__ = 'rocky'
+# from bs4 import BeautifulSoup
+# import urllib2, sys, StringIO, gzip, time, random, re, urllib, os, codecs
+#
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+# f = codecs.open("html.txt", 'r')
+# stream = f.read()
+# print(stream)
+# url = "http://photo.xitek.com/photoid/739530"
+# req = urllib2.Request(url)
+# resp = urllib2.urlopen(req)
+# stream_resp = resp.read()
+# data = StringIO.StringIO(stream_resp)
+# gzipper = gzip.GzipFile(fileobj=data)
+# html = gzipper.read()
+# bs = BeautifulSoup(html, 'html.parser')
+# title = bs.title.string.strip()
+# f = open(title, 'w')
+# f.write("Help")
+# f.close()
+# #temp=unicode(title,'utf-8')
+# #temp=title.decode('utf-8','ignore')
+# a = "hello"
+# print(type(title))
+# print(type(a))
+# #print(title)
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
-f = codecs.open("html.txt", 'r')
-stream = f.read()
-print(stream)
-url = "http://photo.xitek.com/photoid/739530"
-req = urllib2.Request(url)
-resp = urllib2.urlopen(req)
-stream_resp = resp.read()
-data = StringIO.StringIO(stream_resp)
-gzipper = gzip.GzipFile(fileobj=data)
-html = gzipper.read()
-bs = BeautifulSoup(html, 'html.parser')
-title = bs.title.string.strip()
-f = open(title, 'w')
-f.write("Help")
-f.close()
-#temp=unicode(title,'utf-8')
-#temp=title.decode('utf-8','ignore')
-a = "hello"
-print(type(title))
-print(type(a))
-#print(title)
+
+# python3
+
